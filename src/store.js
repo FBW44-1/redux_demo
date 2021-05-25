8bdc52609d8f76c872e2f513ccd57a3c702330bc
HsnAkk <hasan.akkas@web.de>
2021-05-25 16:27:48 +0200

    last updates

--- src/store.js
+++ src/store.js
@@ -2,10 +2,11 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(logger),
+    applyMiddleware(thunk, logger),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 
